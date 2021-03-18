3deda636d04c7da8f3377219b5cbf20147bedb1c
MTTcoin <info@mttcoin.com>
2021-03-18 10:18:12 +0800

    update build files

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -52,8 +52,8 @@ QString BitcoinUnits::description(int unit)
     switch(unit)
     {
     case BTC: return QString("MTTcoins");
-    case mBTC: return QString("Lites (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Photons (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case mBTC: return QString("MTTcent (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("MTTmils (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
